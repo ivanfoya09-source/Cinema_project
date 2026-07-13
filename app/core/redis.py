a67644268e35776dcca13a9701690d761dba5211
Ivan Foya <ivanfoya@gmail.com>
2026-07-13 21:47:32 +0300

    update

--- app/core/redis.py
+++ app/core/redis.py
@@ -4,14 +4,11 @@ from app.core.config import settings
 
 
 if settings.REDIS_URL:
-
     redis_client = redis.from_url(
         settings.REDIS_URL,
         decode_responses=True,
     )
-
 else:
-
     redis_client = redis.Redis(
         host=settings.REDIS_HOST,
         port=settings.REDIS_PORT,
